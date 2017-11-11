584c24b80f4d44cdde756c9164499c79fcb785ff
aConLaptop <tronyeu0000@gmail.com>
2017-11-11 23:01:27 +0000

    use requests package for now to query GITHUB datas

--- Python-bbe/GithubTest.py
+++ Python-bbe/GithubTest.py
@@ -1,27 +1,35 @@
 from github import Github
 import requests, json
+import os
 
-g = Github()
-print(g.rate_limiting)
+# g = Github()
+# print(g.rate_limiting)
+# repos = g.search_repositories('language:php',sort='stars')
 
-
-def getComposerJson(name):
+def downloadComposerJson(name):
     r = requests.get('https://raw.githubusercontent.com/' + name + '/master/composer.json')
     if r.status_code != 404:
         return r.json()
     else:
         return 404
 
-with open('names.json') as data_file:
-    namesData = json.load(data_file)
+def writeComposer(names,directory="composer"):
+    if(not os.path.isdir(directory)):
+        os.makedirs(directory)
+    for name in names:
+        composerFile = downloadComposerJson(name)
+        if composerFile != 404:
+            with open('composer/'+name.rsplit('/', 1)[-1]+".json", 'w') as outfile:
+                json.dump(composerFile, outfile)
+    print("Finished")
 
-noComposer = []
-for name in namesData:
-    composerFile = getComposerJson(name)
-    if composerFile != 404:
-        # with open('composer/'+name.rsplit('/', 1)[-1]+".json", 'w') as outfile:
-        #     json.dump(composerFile, outfile)
-        0
-    else:
-        noComposer.append(name.rsplit('/', 1)[-1])
-print(noComposer)
+
+items = requests.get('https://api.github.com/search/repositories?q=language%3Aphp+sort%3Astars&per_page=100').json()["items"]
+names = []
+for item in items:
+    names.append(item["full_name"])
+writeComposer(names)
+
+
+# with open('names.json') as data_file:
+#     namesData = json.load(data_file)
