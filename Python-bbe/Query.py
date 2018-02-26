20fb25844fecb73bcfc2811b8ae2a1d1a221f857
aConLaptop <hoangtkc95@gmail.com>
2018-02-26 03:00:59 +0000

    project diary, optimize aql queries

--- Python-bbe/Query.py
+++ Python-bbe/Query.py
@@ -3,15 +3,29 @@ from pyArango.collection import Collection, Field, Edges
 from pyArango.graph import Graph, EdgeDefinition
 
 
-
-
-def getTopLibraries(db, date, numberOfLibraries):
+def getTopLibraries(db, graph, libraries, date, numOfLibs):
     # aql_count = "LET count = ( FOR v IN 2 INBOUND 'libraries/php' GRAPH 'github_test' COLLECT usage = v._key RETURN usage ) RETURN LENGTH(count)"
 
-    aql_rank = "FOR library IN libraries LET count = LENGTH(( FOR v, e IN 2 INBOUND library GRAPH 'github_test' FILTER DATE_DIFF(e._to.date,"+date+", 'd', true) > 0 RETURN DISTINCT v )) SORT count DESC LIMIT "+ str(numberOfLibraries) + " RETURN{ 'count': count, 'library':library._key} "
-    queryResult = db.AQLQuery(aql_rank, rawResults=True, batchSize=20)  # Batch size = top 20
+    aql_rank = "FOR library IN @@libraries " \
+               "LET count = LENGTH(( FOR v, e, p IN 2 INBOUND library GRAPH @graph FILTER DATE_DIFF(p[0].date, @date, 'd', true) > 0 RETURN DISTINCT v )) " \
+               "SORT count " \
+               "DESC LIMIT @numberOfLibraries " \
+               "RETURN{ 'count': count, 'library':library._key} "
+    bindVars = {"@libraries": libraries,
+                "graph": graph,
+                "date": date,
+                "numberOfLibraries": int(numOfLibs)}
+    queryResult = db.AQLQuery(aql_rank, bindVars=bindVars, rawResults=True)
 
     return queryResult
+
+
+def getDependencies(db, date):
+    aql_dependencies = "FOR library IN libraries LET count = LENGTH(( FOR v, e IN 2 INBOUND library GRAPH 'github_test' FILTER DATE_DIFF(e.date," + date + ", 'd', true) > 0 RETURN DISTINCT v )) SORT count DESC LIMIT " + numberOfLibraries + " RETURN{ 'count': count, 'library':library._key} "
+    queryResult = db.AQLQuery(aql_dependencies, bindVars={"id": 123, }, rawResults=True)
+
+    return queryResult
+
 
 if __name__ == "__main__":
     from pyArango.connection import *
@@ -19,4 +33,4 @@ if __name__ == "__main__":
     conn = Connection(username="root", password="root")
     db = conn["test_fetch"]
 
-    print(getTopLibraries(db, date = "'2013-10-01'", numberOfLibraries=10))
+    print(getTopLibraries(db, graph='github_test', libraries='libraries', date='2016-10-27', numOfLibs=10))
